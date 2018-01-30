9ff69cf61407a70bf00bfcbee9796ff379383773
Jiashu Wang <jwang20@csl09-l.cornellcollege.edu>
2018-01-30 09:36:20 -0600

    range of alphabet

--- filter.c
+++ filter.c
@@ -9,26 +9,25 @@ int main( int argc, char** argv ) {
   char *line = NULL;
   size_t len = 0;
   ssize_t read;
-
+  int letters_range[26] = {0};
   int letters[26] = {0};
-
+  int sum = 0;
+  int check = 0;
   //printf( "Enter string below [ctrl-D to quit]\n" );
 
   FILE * inputStream = fopen( "tale-of-two-cities.txt", "r" );
 
   while( (read = getline(&line, &len, inputStream)) != -1 ) {
-    
     if( read > 0 ) {
-      
       for (int i = 0; i < strlen(line); i++) {
-	char c = tolower(line[i]);
-	
-	if ('a' <= c && c <= 'z') {
-	  printf ("%c", c);
-          letters[c-'a']++;
-	} //if
+	       char c = tolower(line[i]);
+	        if ('a' <= c && c <= 'z') {
+	           printf ("%c", c);
+             letters[c-'a']++;
+             check++;
+	        } //if
       } //for
-      
+
       printf( "\n=> %s\n", line );
     } // if
 
@@ -41,7 +40,11 @@ int main( int argc, char** argv ) {
   free( line );
   for(int i = 0; i < 26; i++){
     printf("%c = %d\n", 'a' + i, letters[i]);
+    sum = sum + letters[i];
+    letters_range[i] = sum;
+    printf("%c = %d\n", 'a' + i, letters_range[i]);
   }
+  printf("%d\n", check);
   return 0;
 
 } // main( int, char** )
