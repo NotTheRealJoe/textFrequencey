e7ee99afa9b62a3b6ec0a85ed4ee060938322a83
Joseph Tortorello <32490063+NotTheRealJoe@users.noreply.github.com>
2018-02-02 03:34:17 -0600

    Continued implementing hashtable functionality

--- wordFrequency.c
+++ wordFrequency.c
@@ -2,76 +2,147 @@
 #include <stdio.h>
 #include <string.h>
 #include <stdlib.h>
+#include <sys/types.h>
+#include <sys/stat.h>
+#include <fcntl.h>
+#include <unistd.h>
 #define DEBUG 1
 
-hsearch_data* wordFrequency(char* path) {
+hsearch_data wordFrequency(char* path) {
+  //Utility variables
+  int r, newValue;
+  ENTRY e, *ep;
+  char* temp;
+  
+  //First, get the number of words in the book to properly allocate the hash table
+  int words = wordCount(path);
+
   //Open the file as a stream and check for errors
   FILE *stream;
-  stream = fdopen(fd, "r");
-
+  stream = fopen(path, "r");
   if(!stream) {
     perror("fopen");
   }
   if(DEBUG) printf("stream opened successfully\n");
-  
-  int c;
-  char* word = '\0';
-
-  //First, get the number of words in the book to properly allocate the hash table
-  if(DEBUG) printf("begin wordCount...\n");
-  int words = wordCount(fd);
-  if(DEBUG) printf("wordCount success. %i words.\n", words);
 
   //Create the hash table for storing the words
-  //This version of hcreate() allows use of only one hash table at a time but that's ok because this implmentation needs only one
-  ENTRY newEntry;
-  if(DEBUG) printf("newEntry allocation success.\n");
-  
-  ENTRY *newEntry_p;
-  if(DEBUG) printf("newEntry_p allocation success.\n");
-  
-  hsearch_data *htab;
+  struct hsearch_data htab;
   if(DEBUG) printf("*htab allocation success.\n");
-  
-  hcreate_r(words, htab);
+  memset((void *)&htab, 0, sizeof(htab));
+  r = hcreate_r(words, &htab);
+  if(r == 0) {
+    perror("hcreate_r");
+  }
   if(DEBUG) printf("hash table create success.\n");
 
-  c = fgetc(stream);
-  if(DEBUG) printf("First letter is %c\n", c);
+  //Get a character from the file
+  int c;
+  c = fgetc(stream);  
   if(c == EOF) {
     printf("ERROR: First character in file is EOF!\n");
     exit(1);
   }
 
+  //Store the current word in the process of being built
+  char* word  = (char*)calloc(64, sizeof(char));
+  //This flag is set to true as soon as we encounter a valid letter character
+  int isValidWord = 0;
+  //Keep track of our position in the word
+  int index = 0;
   while(c != EOF) {
+    if(DEBUG) printf("Enter loop\n");
+  
     if(c >= 97 && c <= 122) {
+      if(DEBUG) printf("Lower case letter found: %c\n", c);
       //If letter is lower case (ASCII 97 to 122 inclusive), convert to upper and add to word
-      *word++ = c-32;
-      *word = '\0';
+      word[index] = (c-32);
+      if(DEBUG) printf("Added to word. Word is now %s\n", word);
+      index++;
+      isValidWord = 1;
       if(DEBUG) printf("Add letter %c\n", c-32);
     } else if (c >= 65 && c <= 122) {
+      if(DEBUG) printf("Upper case letter found: %c\n", c);
       //If letter is upper case (ASCII 65 to 90 inclusive), just add to word
-      *word++ = c;
-      *word = '\0';
+      word[index] = c;
+      if(DEBUG) printf("Added character to word. Word is now %s\n", word);
+      index++;
+      isValidWord = 1;
       if(DEBUG) printf("Add letter %c\n", c);
     } else if (c == 39) {
+      if(DEBUG) printf("Apostrophie found\n");
       //If the character is an apostrophie, ignore it but don't break the word
       // (do nothing)
       if(DEBUG) printf("Apostrophie, continue to next letter\n");
     } else {
       //If the character is anything other than a letter, break the work, or ignore it if the word is 0 letters long
-      if(strlen(word) > 0) {
+      if(DEBUG) printf("Word-breaking character found\n");
+      if(isValidWord > 0) {
 	if(DEBUG) printf("Add word %s\n", word);
-	newEntry.key = word;
-	newEntry.data = 0;
-	//TODO: check hsearch_r for errors
-	hsearch_r(newEntry, ENTER, &newEntry_p, htab);
-	newEntry.data++;
-	word = '\0';
+
+	//e.key = word;
+	//*(int*)e.data = 0;
+	//if(DEBUG) printf("Assigned data...\n");
+	//if(DEBUG) printf("Assigned key and data to curEntry\nKey is %s, data is %i\n", e.key, *(int*)e.data);
+	e.key = word;
+	printf("Key is %s\n", e.key);
+	e.data = malloc(sizeof(int));
+	//Don't know how this works but this guy does: https://stackoverflow.com/questions/1327579/if-i-have-a-void-pointer-how-do-i-put-an-int-into-it
+	*((int*)e.data) = 0;
+	printf("Value is %i\n", *(int *)e.data);
+  
+	r = hsearch_r(e, ENTER, &ep, &htab);
+	if(DEBUG) printf("hsearch successfully completed\n");
+	if(!r) {
+	  perror("hsearch_r");
+	}
+	if(DEBUG) printf("Stored key is %s\n", ep->key);
+	if(DEBUG) printf("Stored value is %i\n", *(int *)(ep->data));
+	//Dereference ep, get the data void pointer, cast it to an int pointer and then dereference the int pointer. Also you have to use += 1 instead of ++ for some reason
+	*((int *)((*ep).data)) += 1;
+	newValue = *(int *)(ep->data);
+	if(DEBUG) printf("Updated value is %i\n", newValue);
+
+	free(word);
+	char* word  = (char*)calloc(64, sizeof(char));
+	isValidWord = 0;
+	index=0;
+      } else {
+	if(DEBUG) printf("No valid characters currently in word. Continuing without adding\n");
       }
     }
+    if(DEBUG) printf("Done, getting next character\n");
     c = fgetc(stream);
   }
+  if(DEBUG) printf("EOF Reached!\n");
+  //TODO: check fclose for errors
+  if(fclose(stream) != 0) {
+    perror("fclose");
+  }
+  if(DEBUG) printf("Closed input file successfully\n");
 
-  return htab;
+  // Wrtie the data to file
+  int outFd = open("htable.dat", O_WRONLY | O_CREAT | O_TRUNC, 0600);
+  if(outFd == -1) {
+    perror("open");
+  }
+
+  printf("%lu\n", sizeof(htab));
+  
+  if(write(outFd, &htab, sizeof(htab)) == -1) {
+    perror("write");
+  }
+
+  if(close(outFd) == -1) {
+    perror("close");
+  }
+  
+  printf("%lu\n", sizeof(hlist));
+  for(int i = 0; i < words; i++) {
+    if(hlist[i] != NULL) {
+      //      printf("%s => %i\n", hlist[i]->key, *(int *)(hlist[i]->data));
+      printf("%s\n", hlist[i]->key);
+    }
+  }
+  
+  //hdestroy_r(&htab);
 }
--- wordFrequencyTester.c
+++ wordFrequencyTester.c
@@ -1,38 +1,18 @@
 #define _GNU_SOURCE
 #include <stdio.h>
-#include <sys/types.h>
-#include <sys/stat.h>
-#include <fcntl.h>
-#include <unistd.h>
 #include "wordFrequencyTester.h"
-#define DEBUG 0
+#define DEBUG 1
 
-void main(int argc, char** argv) {
-  //int inFd = open("in/twain_mark/74-0.txt", O_RDONLY);
-  //if(inFd < 0) {
-  //  perror("open");
-  //}
-  //if(DEBUG) printf("inFd open success\n");
-  
-  hsearch_data htab;
+void main(int argc, char** argv) {  
+  struct hsearch_data htab;
   if(DEBUG) printf("htab allocation success\n");
   
   hsearch_data *htab_p;
   if(DEBUG) printf("htab_p allocation success\n");
   
-  htab_p = wordFrequency("in/twain_mark/74-0.txt");
+  //htab = wordFrequency("in/twain_mark/74-0.txt");
+  htab = wordFrequency("wordCountTester.in");
   if(DEBUG) printf("htab_p = wordFrequency() success\n");
-  
-  if(close(inFd) < 0) {
-    perror("close");
-  }
 
-  // Wrtie the data to file
-  int outFd = open("htable.dat", O_WRONLY | O_CREAT | O_TRUNC);
-  //TODO: Error check on write()
-  write(outFd, htab_p, sizeof(*htab_p));
-  //TODO: Error check on close()
-  close(outFd);
 
-  //Close the stream
 }
